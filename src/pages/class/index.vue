bbf7fb8dbf3f8fb49c5c5e86498f37ea5d07565b
DeepKolos <deepkolos@qq.com>
2019-05-29 11:11:50 +0800

    border-radiusbug

--- src/pages/class/index.vue
+++ src/pages/class/index.vue
@@ -1,20 +1,41 @@
 <style>
 .test {
-  margin: 100px;
+  margin: 50px;
   background-color: aqua;
 }
 
 .loading {
   background-color: blueviolet;
 }
+
+.border-bug {
+  flex-direction: column;
+  border-radius: 100px;
+  background-color: chartreuse;
+}
+
+.simple-blk {
+  width: 100%;
+  height: 50px;
+  background-color: cyan;
+}
 </style>
 
 <template>
-  <div style="flex-direction: column;">
-    <test-class class="static-class" :class="`test ${state}`" :style="`background-color: ${bgColor}`" />
-    <test-class :style="{
-      backgroundColor: `#ff${index * 3}${index * 3}00`
-    }" v-for="(index) in 3" :key="index" />
+  <div class="border-bug">
+    <div class="simple-blk"></div>
+    <test-class
+      class="static-class"
+      :class="`test ${state}`"
+      :style="`background-color: ${bgColor}`"
+    />
+    <test-class
+      :style="{
+        backgroundColor: `#ff${index * 3}${index * 3}00`
+      }"
+      v-for="index in 3"
+      :key="index"
+    />
   </div>
 </template>
 
